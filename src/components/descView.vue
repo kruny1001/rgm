47addf0b1ed845e98ea3ddf6765c65cc7a77e457
Kevin <eunwoo.son@henergy.xyz>
2019-10-05 14:24:57 +0900

    fix file path

---
+++ src/components/descView.vue
@@ -0,0 +1,19 @@
+<template>
+    <q-page class="descContainer">
+        <h3> Description Page </h3>
+        <p> bla bal </p>
+    </q-page>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.descContainer{
+    position:fixed;
+    width: 100%;
+    height: 100vh;
+    background: yellow;
+}
+</style>
--- src/pages/game12.vue
+++ src/pages/game12.vue
@@ -1,10 +1,44 @@
 <template>
   <q-page class="">
-    
+    <desc-view class="desc-view"></desc-view>
+    <div> 망토색칠</div>
+    <div id="debugTool">
+      <q-btn @click="showDesc()"> Show Desc</q-btn>
+      <q-btn @click="hideDesc()"> Hide Desc</q-btn>
+    </div>
   </q-page>
 </template>
 <script>
+import descView from 'src/components/descView.vue'
+import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
 export default {
+  components: {
+      descView
+  },
+  methods:{
+    
+    hideDesc(){
+      TweenMax.to('.desc-view', 0.5, {
+        css: { x: 150, y: -150 },
+        onComplete: ()=>{ // this.destroy()
+        }
+      })
+      TweenMax.to('.desc-view', 0.3, {opacity:0})
+    },
+    showDesc(){
+      TweenMax.to('.desc-view', 0.5, {
+        css: {
+          x: 0,
+          y: 0
+        },
+        onComplete: ()=>{
+          // this.destroy()
+        }
+      })
+      TweenMax.to('.desc-view', 0.3, {opacity:1})
+    }
+  }
+
   
 }
 </script>
